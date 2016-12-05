6074d5346a93494cbbd3189d253756b1c609324b
Cristina <cristinaguerreroflores@gmail.com>
2016-12-05 10:45:39 +0100

    5_dic_2016,

---
+++ updateuploadall.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+git add --all
+today=`date`
+upddate=`echo $today | cut -d " " -f2-4| tr ' ' '_'`
+echo "Commit: "$upddate
+git commit -m "$upddate"
+git push
+#git push --set-upstream origin gh-pages # -u origin master
