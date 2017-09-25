14dbc3cd0406d800d8740c186386575bd7e45393
Cristina Guerrero <cristinaguerrero@Cristinas-MacBook-Pro.local>
2017-09-25 14:22:04 +0200

    update

--- updateuploadall.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-git add --all
-today=`date`
-upddate=`echo $today | cut -d " " -f2-4| tr ' ' '_'`
-echo "Commit: "$upddate
-git commit -m "$upddate"
-git push
-#git push --set-upstream origin gh-pages # -u origin master
-#tini..e - cristinagf
